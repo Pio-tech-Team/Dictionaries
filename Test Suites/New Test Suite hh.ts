d890eba0a64eb352088e4bd4e489b3da78cc940b
a.areiqat <a.areiqat@areiqat-pc.mshome.net>
2021-07-14 14:45:14 +0300

    Dicts test suite has been Deleted

---
+++ Test Suites/New Test Suite hh.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite hh</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fc46e96b-8c4d-4800-afb9-73651db8021d</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2077e30-15a3-4263-b57b-66491cc12f63</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2- Groups Managment (Dictionaries)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
